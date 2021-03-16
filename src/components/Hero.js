3b8d9c082e9b556c98e47ea615d853dcf420e25a
edjunma <edjunma@gmail.com>
2021-03-15 23:47:03 -0400

    work on homepage hero and styles

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,6 +7,13 @@ const Hero = () => {
       <HeroBg>
         <VideoBg src="" type="video/mp4" />
       </HeroBg>
+      <HeroContent>
+        <HeroItems>
+          <HeroH1>Unreal Destinations</HeroH1>
+          <HeroP>Out of this world</HeroP>
+          <Button>Travel Now</Button>
+        </HeroItems>
+      </HeroContent>
     </HeroContainer>
     )
 }
@@ -17,7 +24,20 @@ const HeroContainer = styled.div`
   background: #0c0c0c;
   display: flex;
   justify-content: center;
+  align-items: center;
+  height: 100vh;
+  padding: 0 1rem;
+  position: relative;
+  color: #fff;
 `
 const HeroBg = styled.div``
 
 const VideoBg = styled.video``
+
+const HeroContent = styled.div``
+
+const HeroItems = styled.div``
+
+const HeroH1 = styled.h1``
+
+const HeroP = styled.p``
