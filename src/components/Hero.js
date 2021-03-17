402f48f5ae9aee7dc9a4186f9eee38f9005545c9
edjunma <edjunma@gmail.com>
2021-03-17 00:28:04 -0400

    finish hero styles

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -48,12 +48,38 @@ const VideoBg = styled.video`
   width: 100%;
   height: 100%;
   -o-object-fit: cover;
+  object-fit: cover;
 `
 
-const HeroContent = styled.div``
+const HeroContent = styled.div`
+  z-index: 3;
+  height: calc(100vh - 80px);
+  max-height: 100%;
+  padding: 0rem calc((100vw - 1300px) / 2);
+`
 
-const HeroItems = styled.div``
+const HeroItems = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  height: 100vh;
+  max-height: 100%;
+  padding: 0;
+  color: #fff;
+  line-height: 1.1;
+  font-weight: bold;
+`
 
-const HeroH1 = styled.h1``
+const HeroH1 = styled.h1`
+  font-size: clamp(1.5rem, 6vw, 4rem);
+  margin-bottom: 1.5rem;
+  letter-spacing: 3px;
+  padding: 0 1rem;
+`
 
-const HeroP = styled.p``
+const HeroP = styled.p`
+  font-size: clamp(1rem, 3vw, 3rem);
+  margin-bottom: 2rem;
+`
