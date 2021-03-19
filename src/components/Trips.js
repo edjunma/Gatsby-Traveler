15be611c3af2ed1b1dce6bdb0de7ed279ad9ae35
edjunma <edjunma@gmail.com>
2021-03-18 22:15:18 -0400

    add more styles to trips and product page

--- src/components/Trips.js
+++ src/components/Trips.js
@@ -15,6 +15,15 @@ export default Trips;
 const ProductsContainer = styled.div`
   min-height: 100vh;
   padding: 5rem calc((100vw - 1300px) /2);
+  background: red;
+  color: #fff;
+`
+const ProductsHeading = styled.div`
+  font-size: clamp(1.2rem, 5vw, 3rem);
+  text-align: center;
+  margin-bottom: 5rem;
+  color: #000;
 `
 
-const ProductsHeading = styled.div``
+const ProductWrapper = styled.div`
+`
