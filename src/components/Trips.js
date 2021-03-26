406e215ff90f6776d3c540d9dc3c3f1dbf2f215d
edjunma <edjunma@gmail.com>
2021-03-26 00:57:05 -0400

    finish trip page styles and add buttons

--- src/components/Trips.js
+++ src/components/Trips.js
@@ -5,7 +5,7 @@ import Img from 'gatsby-image';
 import { Button } from "./Button";
 import { ImLocation } from "react-icons/im";
 
-const Trips = () => {
+const Trips = ({ heading }) => {
   const data = useStaticQuery(graphql`
     allTripsJson {
       edges {
@@ -36,7 +36,7 @@ const Trips = () => {
               <ImLocation />
               <ProductTitle>{item.node.name}</ProductTitle>
             </TextWrap>
-            <Button to="/trips">{item.node.button}</Button>
+            <Button to="/trips" primary="true" round="true" css={`position: absolute; top: 420px; font-size: 14px`}>{item.node.button}</Button>
           </ProductInfo>
         </ProductCard>
       )
@@ -46,7 +46,7 @@ const Trips = () => {
 
   return (
     <ProductsContainer>
-      <ProductsHeading>Heading</ProductsHeading>
+      <ProductsHeading>{heading}</ProductsHeading>
       <ProductWrapper>{getTrips(data)}</ProductWrapper>
     </ProductsContainer>
     )
@@ -57,7 +57,6 @@ export default Trips;
 const ProductsContainer = styled.div`
   min-height: 100vh;
   padding: 5rem calc((100vw - 1300px) /2);
-  background: red;
   color: #fff;
 `
 const ProductsHeading = styled.div`
@@ -107,4 +106,24 @@ const ProductImg = styled(Img)`
 
 const ProductInfo = styled.div`
   display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 0 2rem;
+
+  @media screen and (max-width: 280px) {
+    padding: 0 1rem;
+  }
+`
+
+const TextWrap = styled.div`
+  display: flex;
+  align-items: center;
+  position: absolute;
+  top: 375px;
+`
+
+const ProductTitle = styled.div`
+  font-weight: 400;
+  font-size: 1rem;
+  margin-left: 0.5rem;
 `
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <Hero />
-    <Trips />
+    <Trips heading="Our Favorite Destinations" />
   </Layout>
 )
 
