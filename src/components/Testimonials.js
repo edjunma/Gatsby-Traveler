bb6402a902c07ee3df99be07621756fc5c016bbf
edjunma <edjunma@gmail.com>
2021-03-27 15:25:15 -0400

    add Testimonial component content

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import styled from "styled-components"
 import Img from "gatsby-image"
+import { IoMdCheckmarkCircleOutline } from "react-icons/io"
+import { FaRegLightbulb } from "react-icons/fa"
 
 const Testimonials = () => {
   return (
@@ -18,10 +20,20 @@ const Testimonials = () => {
             <h3>Sean Michaels</h3>
             <p> "The greatest experience of my life! It was so much fun exploring the mountains and they made it super easy to book my trip and accommodation."</p>
           </Testimonial>
+          <Testimonial>
+            <FaRegLightbulb />
+            <h3>Sarah Kin</h3>
+            <p> "It was so easy to set up my trip! They answered all my questions right away and gave me the best price out of all the companies I researched."</p>
+          </Testimonial>
         </ColumnOne>
+        <ColumnTwo>
+        image
+        </ColumnTwo>
       </ContentWrapper>
     </TestimonialsContainer>
   )
 }
 
 export default Testimonials
+
+const TestimonialsContainer = styled.div``
