e606c20c9353f6dc04f67a96f2bdad1a44b90c40
edjunma <edjunma@gmail.com>
2021-04-10 21:31:46 -0400

    finish up StatsData and Stats page

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,31 +1,6 @@
 import React from 'react'
 import styled from "styled-components"
-import {GiEarthAmerica} from "react-icons/gi"
-import {MdAirplanemodeActive, MdTimer} from "react-icons/md"
-import {FaMoneyCheck} from "react-icons/fa"
-
-const StatsData = [
-  {
-    icon: (<GiEarthAmerica css={`color: #047bf1;`} />),
-    title: "Over 100 Destinations",
-    desc: "Travel to over 100 unique places"
-  },
-  {
-    icon: (<MdAirplanemodeActive css={`color: #f3a82e;`} />),
-    title: "1 Million Trips Made",
-    desc: "Over 1 million trips completed last year"
-  },
-  {
-    icon: (<MdTimer />),
-    title: "Fastest Support",
-    desc: "Access our support team 24/7"
-  },
-  {
-    icon: (<FaMoneyCheck />),
-    title: "Best Deals",
-    desc: "We offer the best prices"
-  },
-]
+import { StatsData } from "../data/StatsData"
 
 const Stats = () => {
   return (
@@ -87,7 +62,7 @@ const Icon = styled.div`
   margin-bottom: 1rem;
 `
 
-const Title = styled.div`
+const Title = styled.p`
   font-size: clamp(1rem, 2.5vw, 1.5rem);
   margin-bottom: 0.5rem;
 `
