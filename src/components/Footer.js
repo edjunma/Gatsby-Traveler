709eff5ee05165fe7c1a4c9a47cccf8f990cfbc9
edjunma <edjunma@gmail.com>
2021-04-12 22:50:39 -0400

    working on footer styles, finish footer jsx

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,40 @@
 import React from 'react'
+import { Link } from 'gatsby'
+import styled from 'styled-components'
 
-const Footer() {
+const Footer = () => {
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
+    <FooterContainer>
+      <FooterLinksWrapper>
+        <FooterDesc>
+          <h1>Explorix</h1>
+          <p>We strive to create the best experiences for our customers.</p>
+        </FooterDesc>
+        <FooterLinkItems>
+          <FooterLinkTitle>Contact Us</FooterLinkTitle>
+          <FooterLink to="/about">Contact</FooterLink>
+          <FooterLink to="/">Support</FooterLink>
+          <FooterLink to="/">Destinations</FooterLink>
+          <FooterLink to="/">Sponsorships</FooterLink>
+        </FooterLinkItems>
+      </FooterLinksWrapper>
+      <FooterLinksWrapper>
+        <FooterLinkItems>
+          <FooterLinkTitle>Videos</FooterLinkTitle>
+          <FooterLink to="/">Submit Video</FooterLink>
+          <FooterLink to="/">Ambassadors</FooterLink>
+          <FooterLink to="/">Agency</FooterLink>
+          <FooterLink to="/">Influencer</FooterLink>
+        </FooterLinkItems>
+        <FooterLinkItems>
+          <FooterLinkTitle>Social Media</FooterLinkTitle>
+          <FooterLink to="/">Instagram</FooterLink>
+          <FooterLink to="/">Facebook</FooterLink>
+          <FooterLink to="/">YouTube</FooterLink>
+          <FooterLink to="/">Twitter</FooterLink>
+        </FooterLinkItems>
+      </FooterLinksWrapper>
+    </FooterContainer>
   )
 }
 
--- src/components/layout.js
+++ src/components/layout.js
@@ -5,21 +5,12 @@ import { GlobalStyle } from "./styles/GlobalStyles";
 
 const Layout = ({ children }) => {
   return (
-    <FooterContainer>
-      <FooterLinksWrapper>
-        <FooterDesc>
-          <h1>Explorix</h1>
-          <p>We strive to create the best experiences for our customers.</p>
-        </FooterDesc>
-        <FooterLinkItems>
-          <FooterLinkTitle>Contact Us</FooterLinkTitle>
-          <FooterLink to="/about">Contact</FooterLink>
-          <FooterLink to="/">Support</FooterLink>
-          <FooterLink to="/">Destinations</FooterLink>
-          <FooterLink to="/">Sponsorships</FooterLink>
-        </FooterLinkItems>
-      </FooterLinksWrapper>
-    </FooterContainer>
+    <>
+      <GlobalStyle />
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
   )
 }
 
